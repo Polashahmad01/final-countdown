67b2206a5380a16043c299e46847b04b175d1aa6
Polashahmad01 <dev.polashahmad@gmail.com>
2023-11-27 14:14:40 +0600

    Remove TimerChallenge component and ResultModal component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,11 @@
-import { useState, useRef } from "react"
-
 export default function Player() {
-  const input = useRef("")
-  const [enteredPlayerName, setEnteredPlayerName] = useState()
-
-  function handleSubmit() {
-    setEnteredPlayerName(input.current.value)
-    input.current.value = ""
-  }
-
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <h2>Welcome unknown entity</h2>
       <p>
-        <input ref={input} type="text"/>
-        <button onClick={handleSubmit}>Set Name</button>
+        <input type="text" />
+        <button>Set Name</button>
       </p>
     </section>
-  )
+  );
 }
